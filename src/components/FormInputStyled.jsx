17cd3ab2b76ab2662422a334035f5db410c29485
Matilda Söderblom <soderblom.matilda@gmail.com>
2020-09-22 12:02:56 +0200

    Add styling to input

--- src/components/FormInputStyled.jsx
+++ src/components/FormInputStyled.jsx
@@ -2,17 +2,26 @@ import React from "react";
 import styled from "styled-components";
 
 const InputStyled = styled.input`
-  border-radius: 5px;
+  appearance: none;
+  border: none;
+  border-radius: 3px;
+  box-shadow: 0px 2px 7px 1px rgba(0, 0, 0, 0.5);
+  padding: 0.5em 0.8em;
+`;
+const LabelStyled = styled.label`
+  color: black;
+  display: block;
 `;
 
 export default function FormInputStyled({
+  label,
   placeholder,
   stateVariable,
   stateSetVariable,
 }) {
   return (
     <div>
-      <label>{placeholder} </label>
+      <LabelStyled>{label} </LabelStyled>
       <InputStyled
         placeholder={placeholder}
         value={stateVariable}
