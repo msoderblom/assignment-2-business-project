8495d467f09ad9daad54171723534b7715fb6c4d
Matilda Söderblom <soderblom.matilda@gmail.com>
2020-09-25 18:39:18 +0200

    Add validation for vatNr and add error messages

--- src/components/FormCreateCustomer.jsx
+++ src/components/FormCreateCustomer.jsx
@@ -5,17 +5,22 @@ import * as yup from "yup";
 import UserKit from "../data/UserKit";
 import { CustomerListContext } from "../contexts/CustomerListContext";
 
+const vatNrRegex = RegExp(/^(SE)?[0-9]{12}$/);
+
 const schema = yup.object().shape({
   name: yup.string().required(),
   paymentTerm: yup.number().required(),
   email: yup.string().email(),
+  vatNr: yup
+    .string()
+    .matches(vatNrRegex, "VAT Nr must follow this fotmat: SE999999999901"),
   /* website: yup.string().url(), */
 });
 
 export default function FormCreateCustomer() {
   const userKit = new UserKit();
   const { setCustomerList } = useContext(CustomerListContext);
-  const { register, handleSubmit } = useForm({
+  const { register, handleSubmit, errors } = useForm({
     resolver: yupResolver(schema),
   });
 
@@ -34,18 +39,21 @@ export default function FormCreateCustomer() {
     <form onSubmit={handleSubmit(onSubmit)}>
       <label>Name</label>
       <input name="name" ref={register} />
+      <p>{errors.name?.message}</p>
 
       <label>Organization Number</label>
       <input name="organisationNr" ref={register} />
 
       <label>VAT identification number</label>
       <input name="vatNr" ref={register} />
+      <p>{errors.vatNr?.message}</p>
 
       <label>Reference</label>
       <input name="reference" ref={register} />
 
       <label>Payment Term (days)</label>
       <input name="paymentTerm" type="number" ref={register} />
+      <p>{errors.paymentTerm?.message}</p>
 
       <label>Website</label>
       <input
