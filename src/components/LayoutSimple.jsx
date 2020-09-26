ab3bfd9a51eeb0c89a8e9c094ccf3baea50aea33
Matilda Söderblom <soderblom.matilda@gmail.com>
2020-09-26 16:29:48 +0200

    Add new layoutSimple

---
+++ src/components/LayoutSimple.jsx
@@ -0,0 +1,31 @@
+import React, { useContext } from "react";
+import styled from "styled-components";
+import { UserContext } from "../contexts/UserContext";
+
+const MainContent = styled.main`
+  padding: 1rem;
+`;
+
+export default function LayoutSimple({ children }) {
+  const { user } = useContext(UserContext);
+  return (
+    <div>
+      {user ? (
+        <header>
+          <h1>Business Project</h1>
+          <div>
+            <p>
+              Signed in as {user.firstName} {user.lastName}
+            </p>
+          </div>
+        </header>
+      ) : (
+        <header>
+          <h1>Business Project</h1>
+        </header>
+      )}
+
+      <MainContent>{children}</MainContent>
+    </div>
+  );
+}
