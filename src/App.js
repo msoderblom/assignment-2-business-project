863cc35d301e01078bdd4b6fd8fa81cacfeb1b32
Matilda Söderblom <soderblom.matilda@gmail.com>
2020-09-26 16:02:39 +0200

    Add UserContext

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Switch, Route } from "react-router-dom";
 /* import LayoutAdmin from "./components/LayoutAdmin"; */
 import { CustomerListContext } from "./contexts/CustomerListContext";
+import { UserContext } from "./contexts/UserContext";
 import CustomerDetailPage from "./pages/CustomerDetailPage";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
@@ -11,17 +12,21 @@ import { GlobalStyles } from "./styles/GlobalStyles";
 
 function App() {
   const [customerList, setCustomerList] = useState(null);
+  const [user, setUser] = useState(null);
+
   return (
     <div>
       <GlobalStyles></GlobalStyles>
       <CustomerListContext.Provider value={{ customerList, setCustomerList }}>
-        <Switch>
-          <Route path="/customer/:id" component={CustomerDetailPage} />
-          <Route path="/home" component={HomePage} />
-          <Route path="/login" component={LoginPage} />
-          <Route path="/register" component={RegisterPage} />
-          <Route path="/" component={StartPage} />
-        </Switch>
+        <UserContext.Provider value={{ user, setUser }}>
+          <Switch>
+            <Route path="/customer/:id" component={CustomerDetailPage} />
+            <Route path="/home" component={HomePage} />
+            <Route path="/login" component={LoginPage} />
+            <Route path="/register" component={RegisterPage} />
+            <Route path="/" component={StartPage} />
+          </Switch>
+        </UserContext.Provider>
       </CustomerListContext.Provider>
     </div>
   );
