6039913245d4d68435160ce6c53dd7b2f5293bba
Matilda Söderblom <soderblom.matilda@gmail.com>
2020-09-28 17:46:42 +0200

    Change styling on button

--- src/components/ButtonStyled.jsx
+++ src/components/ButtonStyled.jsx
@@ -3,18 +3,22 @@ import styled from "styled-components";
 
 const Button = styled.button`
   appearance: none;
-  background-color: ${(props) => props.theme.summerSky};
+  background: rgb(225, 115, 97);
+  background: linear-gradient(
+    90deg,
+    rgba(225, 115, 97, 1) 25%,
+    rgba(209, 76, 137, 1) 70%
+  );
   border: none;
-  border-radius: 5px;
-  box-shadow: 0px 2px 7px 1px rgba(0, 0, 0, 0.5);
+  border-radius: 50px;
+  /*   box-shadow: 0px 2px 7px 1px rgba(0, 0, 0, 0.5); */
   color: white;
   font-size: 0.8em;
   padding: 0.5em 0.8em;
-  transition: background-color 0.25s, color 0.25s;
+  transition: all 0.2s ease-in-out;
 
   &:hover {
-    color: ${(props) => props.theme.summerSky};
-    background-color: white;
+    transform: scale(1.03);
     cursor: pointer;
   }
   &:focus {
