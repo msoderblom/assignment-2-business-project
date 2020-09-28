507a53ff9136f9822f938f7b7b1b187897856de5
Matilda Söderblom <soderblom.matilda@gmail.com>
2020-09-28 16:21:01 +0200

    Begin to style header

--- src/components/LayoutSimple.jsx
+++ src/components/LayoutSimple.jsx
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 import styled from "styled-components";
 import { UserContext } from "../contexts/UserContext";
 
+import { RiUser3Fill } from "react-icons/ri";
+
 const MainContent = styled.main`
   padding: 0.5rem;
 
@@ -10,24 +12,53 @@ const MainContent = styled.main`
   }
 `;
 
+const HeaderStyled = styled.header`
+  padding: 1em;
+  background-color: ${(props) => props.theme.christalle};
+  color: white;
+  display: flex;
+  justify-content: space-between;
+`;
+const Heading = styled.h1`
+  font-size: 1em;
+
+  @media only screen and (min-width: 600px) {
+    font-size: 2em;
+  }
+`;
+
+const UserWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 12px;
+`;
+const IconWrapper = styled.div`
+  padding: 5px 10px;
+  line-height: 0;
+`;
+
 export default function LayoutSimple({ children }) {
   const { user } = useContext(UserContext);
   return (
     <div>
-      {user ? (
-        <header>
-          <h1>Business Project</h1>
-          <div>
-            <p>
-              Signed in as {user.firstName} {user.lastName}
-            </p>
-          </div>
-        </header>
-      ) : (
-        <header>
-          <h1>Business Project</h1>
-        </header>
-      )}
+      <HeaderStyled>
+        <Heading>Business Management</Heading>
+        <UserWrapper>
+          {user && (
+            <>
+              <IconWrapper>
+                <RiUser3Fill size={30} />
+              </IconWrapper>
+              <div>
+                <p>
+                  {user.firstName} {user.lastName}
+                </p>
+                <p>{user.email}</p>
+              </div>
+            </>
+          )}
+        </UserWrapper>
+      </HeaderStyled>
 
       <MainContent>{children}</MainContent>
     </div>
